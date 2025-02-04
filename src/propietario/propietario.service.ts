ec271f7522ea0f5f0b7c01a868103af6b069e0a0
Ing. Armando Campos <armandocamposf@gmail.com>
2025-02-04 12:05:39 -0400

    _fix: busqueda del propietario

--- src/propietario/propietario.service.ts
+++ src/propietario/propietario.service.ts
@@ -48,7 +48,7 @@ export class PropietarioService {
       include: {
         ventas: {
           orderBy: {
-            createdAt: 'desc'
+            createdAt: 'asc'
           }
         }
       }
